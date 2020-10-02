61c9b9c2ae5bb3f468aca9e5fefc5b095a89291f
Don Hamilton III <donhamiltoniii@gmail.com>
2020-10-02 16:34:46 -0400

    feature: add unit tests

---
+++ src/models/Cart/Cart.test.js
@@ -0,0 +1,16 @@
+const Cart = require('./Cart.js');
+
+test('addToCart function can add an item to the cart', () => {
+  const testCart = new Cart();
+  testCart.addToCart('cheesecake');
+
+  expect(testCart.items).toEqual(['cheesecake']);
+});
+
+test('removeFromCart function can remove an existing item from the cart', () => {
+  const testCart = new Cart();
+  testCart.addToCart('cheesecake');
+  testCart.removeFromCart('cheesecake');
+
+  expect(testCart.items).toEqual([]);
+});
